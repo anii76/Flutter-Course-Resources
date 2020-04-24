8a7834dd790d95dc57d4e8476e96ab41cc2c55c4
anii76 <a.bourouina@esi-sba.dz>
2020-04-24 11:30:19 +0100

    i_am_rich app

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900]
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image.asset('images/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
